1132c5a85afe2a495d084de056aa3377ef6783f6
MD Fahad Molla <ahfahad47sss@gmail.com>
2024-12-11 08:33:10 +0600

    backend api complete

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from "dotenv";
+
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use("/api/products",productRoutes);
 
-app.get("/products",(req,res)=>{});
-
-console.log(process.env.MONGO_URI);
-
-app.listen(5000,()=>{
-    console.log("server started at location host 5000")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    connectDB();
+    console.log("server started at http://localhost:"+PORT);
+});
